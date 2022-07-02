e743c63b576558a11d464d83c25b96024740ae0b
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-07-02 10:16:55 -0300

    now apliaction can register new tasks

--- aplication/src/App.tsx
+++ aplication/src/App.tsx
@@ -6,35 +6,65 @@ import clipBoardImage from './assets/Clipboard.svg'
 import { Header } from "./components/Header"
 import { CreateTask } from "./components/CreateTask"
 import { Task } from "./components/Task"
+import {  useState } from "react"
 
+export interface  task {
+  id: number
+  content: string
+  isCompleted: boolean
+}
 
 function App() {
+  const [allTaks, setAllTasks] = useState<task[]>([])
+  const [taskTextContent, setTaskTextContent] = useState('')
+
+  const tasksDone = allTaks.filter(task =>{
+    return task.isCompleted === true
+  })
+
+
 
   return (
     <div className="App">
       <Header/>
-
       <main className={styles.container}>
-        <CreateTask/>
+        <CreateTask
+          setAllTasks = {setAllTasks}
+          taskTextContent = {taskTextContent}
+          setTaskTextContent = {setTaskTextContent}
+        />
         
         <section className={styles.taskStatus}>
           <div className={styles.created}>
             <strong>Tarefas Criadas</strong> <span>0</span>
           </div>
           <div className={styles.done}>
-            <strong> Cuncluídas</strong> <span>0</span>
+            <strong> Cuncluídas</strong> <span>{tasksDone.length}</span>
           </div>
         </section>
 
         <article className={styles.tasks}>
-          { /*<div className={styles.withoutTask}>
-            <img src={clipBoardImage} alt="" />
-            <p>
-              <strong>Você ainda não tem tarefas cadastradas</strong>
-               Crie tarefas e organize seus itens a fazer
-            </p>
-            </div> */}
-            <Task/>
+          { 
+            allTaks.length === 0
+            ?
+              <div className={styles.withoutTask}>
+                <img src={clipBoardImage} alt="" />
+                <p>
+                  <strong>Você ainda não tem tarefas cadastradas</strong>
+                    Crie tarefas e organize seus itens a fazer
+                </p>
+              </div> 
+
+            :
+            allTaks.map(task =>{
+              return(
+                <Task
+                  key={task.id}
+                />
+              )
+            })
+          }
+            
         </article>
 
 
--- aplication/src/components/CreateTask/index.tsx
+++ aplication/src/components/CreateTask/index.tsx
@@ -1,12 +1,53 @@
 import {PlusCircle} from 'phosphor-react'
+import { ChangeEvent,FormEvent,SetStateAction, useState } from 'react'
+import { task } from '../../App'
 import styles from './styles.module.css'
 
-export function CreateTask(){
+interface CreateTaskProps {
+    taskTextContent: string
+    setTaskTextContent: React.Dispatch<SetStateAction<string>>
+    setAllTasks: React.Dispatch<SetStateAction<task[]>>
+}
+
+export function CreateTask({
+    setTaskTextContent,
+    taskTextContent,
+    setAllTasks}:CreateTaskProps){
+
+    const [taskId, setTaskId] = useState(1)
+
+    function handleChangeTaskTextContent(event: ChangeEvent<HTMLTextAreaElement>){
+        setTaskTextContent(event.target.value)
+    }
+
+    function handleAddNewTask(event:FormEvent<HTMLFormElement>){
+        event.preventDefault()
+
+        const newTask = 
+        {
+            id: taskId,
+            content: taskTextContent,
+            isCompleted: false 
+        }
+        
+        setAllTasks(state => {
+            return[...state,newTask]
+        })
+        setTaskId(state => {
+            return state + 1
+        })
+        setTaskTextContent('')
+    
+    }
+
     return(
         <section className={styles.createTaskContainer}>
-            <form>
-                <textarea placeholder='Adicione uma nova tarefa'/>
-                <button>
+            <form onSubmit={handleAddNewTask}>
+                <textarea 
+                    value={taskTextContent}
+                    onChange={handleChangeTaskTextContent}
+                    placeholder='Adicione uma nova tarefa'/>
+                <button type='submit'>
                     <span>Criar</span>
                     <PlusCircle/>
                 </button>
